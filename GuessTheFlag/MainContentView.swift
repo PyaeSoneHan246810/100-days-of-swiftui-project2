9d9d6644cbe721f7b658e73462848d9880cd6d9f
Pyae Sone Han <pyaesonehan246810@gmail.com>
2024-11-06 15:33:25 -0800

    complete day 34 challenge

--- GuessTheFlag/MainContentView.swift
+++ GuessTheFlag/MainContentView.swift
@@ -2,12 +2,17 @@ import SwiftUI
 
 struct MainContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
-    @State private var correntAnswer  = Int.random(in: 0...2)
+    @State private var correctAnswer  = Int.random(in: 0...2)
     @State private var showScoreAlert = false
     @State private var scoreAlertTitle = ""
     @State private var score = 0
     @State private var questionCount = 0
     @State private var showFinalAlert = false
+    @State private var correctRotationAmount = 0.0
+    @State private var correctOpacityAmount = 1.0
+    @State private var incorrectOpacityAmount = 1.0
+    @State private var correctScaleAmount = 1.0
+    @State private var incorrectScaleAmount = 1.0
     var body: some View {
         ZStack {
             RadialGradient(
@@ -33,7 +38,7 @@ struct MainContentView: View {
                         Text("Tap the flag of")
                             .foregroundStyle(.secondary)
                             .font(.subheadline.weight(.heavy))
-                        Text(countries[correntAnswer])
+                        Text(countries[correctAnswer])
                             .foregroundStyle(.primary)
                             .font(.largeTitle.weight(.semibold))
                     }
@@ -42,6 +47,9 @@ struct MainContentView: View {
                             flagTapped(number)
                         } label: {
                             FlagImage(country: countries[number])
+                                .rotation3DEffect(.degrees(number == correctAnswer ? correctRotationAmount : 0.0), axis: (x: 1, y: 1, z: 0))
+                                .opacity(number == correctAnswer ? correctOpacityAmount : incorrectOpacityAmount)
+                                .scaleEffect(number == correctAnswer ? correctScaleAmount : incorrectScaleAmount)
                         }
                     }
                 }.frame(maxWidth: .infinity)
@@ -87,30 +95,40 @@ struct MainContentView: View {
             showFinalAlert = false
             showScoreAlert = true
         }
-        if number == correntAnswer {
+        if number == correctAnswer {
+            correctRotationAmount = 360
+            correctOpacityAmount = 1.0
+            incorrectOpacityAmount = 0.5
+            correctScaleAmount = 1.0
+            incorrectScaleAmount = 0.8
             scoreAlertTitle = "Correct"
             score += 1
         } else {
+            correctOpacityAmount = 1.0
+            incorrectOpacityAmount = 0.5
+            correctScaleAmount = 1.0
+            incorrectScaleAmount = 0.8
             scoreAlertTitle = "Wrong! That's the flag of \(countries[number])"
         }
     }
     
     private func continueGame() {
         countries.shuffle()
-        correntAnswer = Int.random(in: 0...2)
+        correctAnswer = Int.random(in: 0...2)
         showScoreAlert = false
         scoreAlertTitle = ""
         showFinalAlert = false
+        correctRotationAmount = 0
+        correctOpacityAmount = 1.0
+        incorrectOpacityAmount = 1.0
+        correctScaleAmount = 1.0
+        incorrectScaleAmount = 1.0
     }
     
     private func restartGame() {
-        countries.shuffle()
-        correntAnswer = Int.random(in: 0...2)
-        showScoreAlert = false
-        scoreAlertTitle = ""
+        continueGame()
         score = 0
         questionCount = 0
-        showFinalAlert = false
     }
 }
 
